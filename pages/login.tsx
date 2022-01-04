4753b4fe50bb86d69a6ee5be790e0069b10b3828
allilk <milk@cream.glass>
2022-01-04 15:37:05 -0500

    Fix revert

--- pages/login.tsx
+++ pages/login.tsx
@@ -3,7 +3,6 @@ import Head from "next/head";
 import { getProviders, getSession } from "next-auth/react";
 
 import { LoginForm } from "../components/user/login/LoginForm";
-import { useEffect, useState } from "react";
 
 export const getServerSideProps = async (context: any) => {
 	const session = await getSession(context);
@@ -13,26 +12,18 @@ export const getServerSideProps = async (context: any) => {
 
 	return {
 		props: {
-			// providers: await getProviders() || [],
+			providers: await getProviders(),
 			referer: referer || "",
 		},
 	};
 };
 
 interface Props {
+	providers: JSON;
 	referer: string;
 }
 
 const Login: NextPage<Props> = (props) => {
-	const [providers, setProviders] = useState(null);
-
-	useEffect(() => {
-	  (async () => {
-		const res = await getProviders();
-		setProviders(res);
-		console.log(res)
-	  })();
-	}, []);
 
 	return (
 		<>
@@ -40,7 +31,7 @@ const Login: NextPage<Props> = (props) => {
 				<title>Login</title>
 				<meta content="Login" property="og:title" />
 			</Head>
-			<LoginForm referer={props.referer} providers={providers}/>
+			<LoginForm {...props} />
 		</>
 	);
 };
